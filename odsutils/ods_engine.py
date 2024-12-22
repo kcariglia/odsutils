4fb75ca10d4f289acbf8fa739ffdfd4896f51074
DAVID DEBOER <ddeboer@berkeley.edu>
2024-12-22 12:13:38 -0800

    add json input for ods

--- odsutils/ods_engine.py
+++ odsutils/ods_engine.py
@@ -59,6 +59,8 @@ class ODS(tools.Base):
             self.ods_file_name = ods_input
             input_ods_data = tools.read_json_file(self.ods_file_name)
             self.ods = input_ods_data[self.standard.data_key]  # This is the internal list of ods records
+        else:
+            raise ValueError(f"Invalid ODS type {type(ods_input)}")
         self.number_of_records = len(self.ods)
         self.valid_records = self.check.ods(self.ods)
         self.qprint(f"Read {self.number_of_records} records from {self.ods_file_name}")
