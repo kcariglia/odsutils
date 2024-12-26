110b5f0678472ad64a0aa71bc113ee20bb64b0f4
DAVID DEBOER <ddeboer@berkeley.edu>
2024-12-26 15:40:07 -0800

    added record validity check to standard

--- odsutils/ods_standard.py
+++ odsutils/ods_standard.py
@@ -1,3 +1,5 @@
+from . import ods_tools as tools
+
 DEFAULT_WORKING_INSTANCE = 'primary'
 LATEST = 'A'
 PLOT_AZEL = 'Az vs El'
@@ -35,8 +37,6 @@ class Standard_Version_A:
                        'src_id', 'src_is_pulsar_bool', 'corr_integ_time_sec', 'src_ra_j2000_deg', 'src_dec_j2000_deg', 'src_radius',
                        'slew_sec', 'trk_rate_dec_deg_per_sec', 'trk_rate_ra_deg_per_sec', 'freq_lower_hz', 'freq_upper_hz', 'notes']
 
-    meta_fields = {'data_key': 'ods_data',
-                   'time_fields': ['src_start_utc', 'src_end_utc']}
 
     def __init__(self):
         self.transfer_keys = {
@@ -50,6 +50,8 @@ class Standard_Version_A:
             'start': 'src_start_utc',
             'stop': 'src_end_utc'
         }
+        self.meta_fields = {'data_key': 'ods_data',
+                            'time_fields': ['src_start_utc', 'src_end_utc']}
 
 class Standard:
     """
@@ -96,3 +98,51 @@ class Standard:
         self.time_fields = self.standard.meta_fields['time_fields']
         for key, val in self.standard.transfer_keys.items():
             setattr(self, key, val)
+
+    def valid(self, rec):
+        """
+        Checks a single ods record for:
+            1 - keys are all valid ODS fields
+            2 - values are all consistent with ODS field type
+            3 - all fields are present
+            4 - time fields are parseable by astropy.time.Time
+
+        Parameter
+        ---------
+        rec : dict
+            An ods record
+        
+        Return
+        ----------
+        bool
+            Is the record a valid ods record
+        list of str
+            Messages about validity check
+
+        """
+        is_valid = True
+        msg = []
+        for key in rec:  # check that all supplied keys are valid and not None
+            if key not in self.ods_fields:
+                msg.append(f"{key} not an ods_field")
+                is_valid = False
+            elif rec[key] is None:
+                msg.append(f"Value for {key} is None")
+                is_valid = False
+        for key in self.ods_fields:  # Check that all keys are provided for a rec and type is correct
+            if key not in rec:
+                msg.append(f"Missing ODS field {key}")
+                is_valid = False
+            elif rec[key] is not None:
+                try:
+                    _ = self.ods_fields[key](rec[key])
+                except ValueError:
+                    msg.append(f"{rec[key]} is wrong type for {key}")
+                    is_valid = False
+        for key in self.time_fields:
+            try:
+                _ = tools.make_time(rec[key])
+            except ValueError:
+                msg.append(f"{rec[key]} is not a valid astropy.time.Time input format")
+                is_valid = False
+        return is_valid, msg
