01accd0a18ece603401a298117efe49dcf89253c
DAVID DEBOER <ddeboer@berkeley.edu>
2024-12-27 13:43:10 -0800

    add worning check in init

--- odsutils/__init__.py
+++ odsutils/__init__.py
@@ -1,4 +1,9 @@
 from importlib.metadata import version
 __version__ = version('odsutils')
 
+import warnings
+from erfa import ErfaWarning
+with warnings.catch_warnings():
+    warnings.filterwarnings("ignore", category=ErfaWarning)
+
 from . import ods_engine
