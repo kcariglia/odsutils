2721549b3165e8a6ba0a91b662e41ab4c5b3cfe3
DAVID DEBOER <ddeboer@berkeley.edu>
2024-12-27 13:52:55 -0800

    trying to suppress erfa warning still

--- odsutils/__init__.py
+++ odsutils/__init__.py
@@ -1,9 +1,12 @@
 from importlib.metadata import version
 __version__ = version('odsutils')
 
-import warnings
-from erfa import ErfaWarning
-with warnings.catch_warnings():
-    warnings.filterwarnings("ignore", category=ErfaWarning)
+try:
+    import warnings
+    from erfa import ErfaWarning
+    with warnings.catch_warnings():
+        warnings.filterwarnings("ignore")
+except ImportError:
+    pass
 
 from . import ods_engine
